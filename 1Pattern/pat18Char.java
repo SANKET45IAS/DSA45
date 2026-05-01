db70df28f4c9569a26a041452cedabe8f852e6aa
SANKET45IAS <sanket45nayak@gmail.com>
2026-05-01 12:31:34 +0530

    Added DSA pattern files

---
+++ 1Pattern/pat18Char.java
@@ -0,0 +1,11 @@
+public class pat18Char {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){ 
+                  for (int j = n - i; j < n; j++) {
+                System.out.print((char) ('A' + j) + " ");
+            }
+            System.out.println();
+        }  
+    }
+}
