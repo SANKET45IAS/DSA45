282bd4d889c0b47bd02521a0112fed2088d18ec7
SANKET45IAS <sanket45nayak@gmail.com>
2026-05-04 00:56:06 +0530

    Added DSA pattern files

---
+++ SA/P1TotalDiginNum.java
@@ -0,0 +1,13 @@
+package SA;
+
+import java.util.Scanner;
+
+public class P1TotalDiginNum {
+    public static void main(String[] args) {
+        Scanner sa=new Scanner(System.in);
+        System.out.print("Enter a no. to check no. of digits present in it : ");
+        int n=sa.nextInt();
+        System.out.println();
+        System.out.println("No of digits in " +n+ " is "+ (int)(Math.log10(n)));
+    }
+}
