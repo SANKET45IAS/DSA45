744334c5e4cc9bb6037238c5573a01f406123615
SANKET45IAS <sanket45nayak@gmail.com>
2026-05-04 00:57:40 +0530

    Added DSA pattern files

--- SA/P1TotalDiginNum.java
+++ SA/P1TotalDiginNum.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class P1TotalDiginNum {
     public static void main(String[] args) {
-        Scanner sa=new Scanner(System.in);
-        System.out.print("Enter a no. to check no. of digits present in it : ");
-        int n=sa.nextInt();
-        System.out.println();
-        System.out.println("No of digits in " +n+ " is "+ (int)(Math.log10(n)));
+        try (Scanner sa = new Scanner(System.in)) {
+            System.out.print("Enter a no. to check no. of digits present in it : ");
+            int n=sa.nextInt();
+            System.out.println();
+            System.out.println("No of digits in " +n+ " is "+ (int)(Math.log10(n)));
+        }
     }
 }
