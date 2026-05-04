845a3cd59a91c884370d28e5cc1db862e6a45ed3
SANKET45IAS <sanket45nayak@gmail.com>
2026-05-05 01:26:55 +0530

    Added DSA pattern files

---
+++ PPrimeNo.java
@@ -0,0 +1,18 @@
+public class PPrimeNo {
+    public static boolean isPrime(int n){
+        int c=0;
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if (n%i==0) {
+                c++;
+                if (n%i!=i) {
+                    c++;
+                }
+            }
+        }
+        return c==2;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(5));
+        System.out.println(isPrime(9));
+    }
+}
